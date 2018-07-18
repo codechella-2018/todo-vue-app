cf351c8b5c24bccf16ae73594b1833212313a0e9
dani <dani.zaghian@gmail.com>
2018-07-17 19:49:03 -0700

    added google maps

--- public/index.js
+++ public/index.js
@@ -14,6 +14,47 @@ var HomePage = {
       console.log(this.tasks);
     }.bind(this));
   },
+  mounted: function() {
+    var myLatLng = {lat: 37.788, lng: -122.407}
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 11
+    });
+
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Union Square</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Union Square</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Union Square'
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  },
   methods: {
     addTask: function() {
       var clientParams = {
