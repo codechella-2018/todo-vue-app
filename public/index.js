ee1d07981a2db8a4790dd01856521ec23a57d980
dani <dani.zaghian@gmail.com>
2018-07-17 21:08:44 -0700

    added google maps loop

--- public/index.js
+++ public/index.js
@@ -21,38 +21,24 @@ var HomePage = {
       zoom: 11
     });
 
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Union Square</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Union Square</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
+    var places = [
+      {lat: -25.363, lng: 131.044, description: "A place in Australia" },
+      {lat: -33.8675, lng: 151.207, description: "Sydney!" },
+      {lat: 37.788, lng: -122.407, description: "Union City"}
+    ]
 
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Union Square'
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
+    places.forEach(function(place){
+      var marker = new google.maps.Marker({
+        position: place,
+        map: map,
+        title: place.description
+      });
+      var infowindow = new google.maps.InfoWindow({
+        content: place.description
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
     });
   },
   methods: {
